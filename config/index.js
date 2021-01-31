5164b1f24bedfe593951d7a1456161db14be415d
yasin <yasintze@gmail.com>
2021-01-31 18:39:55 +0700

    show fetch data

---
+++ config/index.js
@@ -0,0 +1,3 @@
+const dev = process.env.NODE_ENV !== 'production';
+
+export const host = dev ? 'http://localhost:3000' : 'http://next-movie-gallery.vercel.app';
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,44 @@
+import { Container } from 'semantic-ui-react';
+
+import { host } from '../config';
 import Meta from '../components/Meta';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home({ movies: { data } }) {
     return (
         <div className={styles.container}>
             <Meta />
             <Navbar />
 
-            <main className={styles.main}></main>
+            <Container>
+                <main className={styles.main}>
+                    {data &&
+                        data.map((movie) => {
+                            const { id, title, showTime } = movie;
+                            return (
+                                <div key={id}>
+                                    <h1>{title}</h1>
+                                    <span>{showTime}</span>
+                                </div>
+                            );
+                        })}
+                </main>
+            </Container>
 
             <Footer />
         </div>
     );
+}
+
+export async function getStaticProps() {
+    const res = await fetch(`${host}/api/movies?page=1`);
+    const data = await res.json();
+
+    return {
+        props: {
+            movies: data
+        }
+    };
 }
